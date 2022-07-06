d2445962a0125a1580fed1bbfee2849f9da8b3a9
Mitchel Alejo <michalejo94@gmail.com>
2022-07-06 14:00:02 -0500

    StrangersThingsRep4

--- src/api/index.js
+++ src/api/index.js
@@ -1,44 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react';
+import axios from "axios";
 
-export const cohortName = '2206-FTB-ET-WEB-FT'
-export const BASE_URL =`https://strangers-things.herokuapp.com/api`
+export const cohortName = "2206-FTB-ET-WEB-FT";
+export const BASE_URL = `https://strangers-things.herokuapp.com/api/`;
 
-export async function fetchAllPosts(){
-    try{
-        const response = await fetch(`${BASE_URL}${cohortName}/posts`);
-           const result = await response.json();
-            if (result.error) throw result.error;
-            return result.data.posts;
-    } catch (err){
-        console.error('Uh oh, trouble fetching posts!', err);
-    }
-};
-
-
-function registerPerson(event){
-    const registerUsername = event.target[0].value
-    const registerPassword = event.target[1].value
-
-    console.log(`${BASE_URL}${cohortName}/users/register`)
-    const response = await 
-    fetch(`${BASE_URL}${cohortName}/users/register`,
-    {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-        user:{
-            username: registerUsername,
-            password: registerPassword
-        }
-    })
-})
-const result = await response.json()
-const token = result.data.token
-lovalStorage.setItem("token", token)
-const tokenFromStorage = localStorage.getItem("token")
-console.log(token)  
+export async function getPosts() {
+  try {
+    const data  = await fetch(`${BASE_URL}${cohortName}/posts`);
+    const result = await data.json()
+    return result;
+  } catch (error) {
+    throw error;
+  }
 }
 
+
+// function registerPerson(event){
+//     const registerUsername = event.target[0].value
+//     const registerPassword = event.target[1].value
+
+//     console.log(`${BASE_URL}${cohortName}/users/register`)
+//     const response = await
+//     fetch(`${BASE_URL}${cohortName}/users/register`,
+//     {
+//         method: "POST",
+//         headers: {
+//             "Content-Type": "application/json"
+//     },
+//     body: JSON.stringify({
+//         user:{
+//             username: registerUsername,
+//             password: registerPassword
+//         }
+//     })
+// })
+// const result = await response.json()
+// const token = result.data.token
+// lovalStorage.setItem("token", token)
+// const tokenFromStorage = localStorage.getItem("token")
+// console.log(token)
+// }
--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,16 @@
-import React from 'react';
-
+import React, {useEffect} from 'react';
+import { getPosts } from '../api';
 
 import './App.css';
 import './index.js';
 
 const App = () => {
+    useEffect (()=>{
+        getPosts().then((posts)=>{
+            console.log(posts);
+
+        })
+    })
     return (
     <h1> App Component</h1>)
 }
