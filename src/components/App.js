705db0351c9223bf12bece1dd6576b8a18e0e61c
Ruby Gonzalez <RubyJ.Gonzalez@outlook.com>
2022-07-06 11:15:06 -0500

    Strangers Things 2

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
+
+import './App.css';
+
 const App = () => {
-    return (<h1> App Component</h1>)
+    return (
+    <h1> App Component</h1>)
 }
 
 export default App
--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,4 @@
-export {default as App} from "./App"
+export const cohortName = '2206-FTB-ET-WEB-FT'
+export const BASE_URL =`https://strangers-things.herokuapp.com${cohortName}`
+
+export {default as App} from "./App";
