ef4677d2586172b3e73734932aa172c64399d0b9
Mitchel Alejo <michalejo94@gmail.com>
2022-07-07 15:41:50 -0500

    StrangersThings Working on Register

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+export const cohortName = "2206-FTB-ET-WEB-FT";
+export const BASE_URL = `https://strangers-things.herokuapp.com/api/`;
 
 async function handleSubmit(event){
     event.preventDefault()
@@ -6,6 +8,29 @@ async function handleSubmit(event){
 }
 export default function Register(){
     const[username, setUsername] = useState('')
+    const createUser = async (event)=>{
+        try {
+        event.preventDefault()
+    const response = await fetch (`${BASE_URL}${cohortName}/users/Register`,{
+        method:"POST",
+        headers: {
+            "Content-Type":"application/json"
+        },
+        body:JSON.stringify({
+            user:{
+                username:'username',
+                password: 'password',
+            }
+        })
+    })
+    const result = await response.json()
+    const token = result.data.token
+return result
+
+}catch(error){
+    throw error;
+}
+} 
     return(<>
     <form onSubmit ={handleSubmit}>
         <h1>REGISTER</h1>
