8417790325bf839f3caad553dbc9c5178dd1c217
Ruby Gonzalez <RubyJ.Gonzalez@outlook.com>
2022-07-11 11:55:35 -0500

    Working on sending messages

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,7 @@ import "./App.css";
 const App = () => {
   const [postValue, setPostValue] = useState('')
   const [messages, setMessages] = useState([])
+  const [posts, setPosts] = useState([]);
 
   
   return (
@@ -20,7 +21,7 @@ const App = () => {
       <Routes>
         <Route exact path="/Register" element = {<Register/>}></Route>
         <Route exact path="/Login" element = {<Login/>}></Route>
-        <Route exact path="/Posts" element = {<Posts postValue = {postValue} setPostValue = {setPostValue}/>}></Route>
+        <Route exact path="/Posts" element = {<Posts postValue = {postValue} setPostValue = {setPostValue} posts ={posts} setPosts={setPosts}/>}></Route>
         <Route exact path="/Profile" element = {<Profile messages ={messages} setMessages = {setMessages}/> }></Route>
         <Route exact path = "/NewPost" element = {<NewPost/>}></Route>
         <Route exact path = "/Logout" element = {<Logout/>}></Route>
--- src/components/MessageForm.js
+++ src/components/MessageForm.js
@@ -1,12 +1,20 @@
 import React, { useEffect, useState } from "react";
 import { Route, Link, Routes, useNavigate } from "react-router-dom";
-import { deletePosts, getPosts } from "../api";
+import { deletePosts, getPosts, sendMessage } from "../api";
+import newPost from "./NewPost";
 
-export default function MessageForm(){
+export default function MessageForm(token){
     const [messageContent, setMessageContent] =useState("")
     const navigate = useNavigate ()
-    const handleMessageContent = async (event)
-    return (<form>
+   async function handleMessage(event){
+        event.preventDefault()
+        token = localStorage.getItem("token")
+        const response = await sendMessage(token, postid, content)
+
+    }
+
+
+    return (<form onSubmit ={handleMessage}>
         <h1>Message Form</h1>
         <label>Message</label>
         <input
